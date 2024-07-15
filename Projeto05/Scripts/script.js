c322210df2ad2ad55014d7a3146c894cde5f1e87
Lucas Souza <149397933+LucasSouza67@users.noreply.github.com>
2024-07-15 11:43:22 -0300

    update Projeto 5

--- Projeto05/Scripts/script.js
+++ Projeto05/Scripts/script.js
@@ -11,31 +11,45 @@ function isNumero(n){
         return false
     }
 }
-function inList(n, l){
-    if (l.indexOf(Number(n)) != -1){
-        return true
-    }
-    else{
-        return false
-    }
-}
-
 function adicionar(){
-    if (isNumero(num.value) && !inList(num.value, valores)){
+    if (isNumero(num.value) && !valores.includes(num.value)){
         valores.push(num.value)
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         list.appendChild(item)
+        res.innerHTML = ''
+        num.value = ''
+        num.focus()
     }
     else{
         window.alert('--> [ERRO] <-- Insira um valor válido')
     }
 }
-
-
-
+function soma(){
+    let sum = 0
+    for (let i in valores){
+        sum +=  Number(valores[i])
+    }
+    return sum
+}
+function mediaN(){
+    let sum = 0
+    for (let i in valores){
+        sum +=  Number(valores[i])
+    }
+    return sum / valores.length
+}
 function exibir(){
-    
+    let tam = valores.length
+    let maxValue = Math.max(...valores)
+    let minValue = Math.min(...valores)
+    let media = mediaN()
+    let sum = soma()
+    res.innerHTML = `<p>Número de valores: ${tam}</p>`
+    res.innerHTML += `<p>Maior valor: ${maxValue}</p>`
+    res.innerHTML += `<p>Menor valor : ${minValue}</p>`
+    res.innerHTML += `<p>Soma dos valores: ${sum}</p>`
+    res.innerHTML += `<p>Media dos valores: ${media.toFixed(1)}</p>`
 }
 
 
